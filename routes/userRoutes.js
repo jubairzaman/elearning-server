b1c453ae7eb160fa09544690d8bd256c81348ad3
jubairzaman <jubair.zaman01@gmail.com>
2025-03-05 11:36:41 +0600

    get user file done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,7 @@ import { fetch,create,update, deleteUser } from "../controller/userController.js
 const route = express.Router();
 
 route.post ("/create",create);
-
+route.get("/getAllUsers", fetch)
 
 
 export default route;
