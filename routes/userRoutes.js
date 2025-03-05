d7f906d4085c31670162d114a13d017e9bddb214
jubairzaman <jubair.zaman01@gmail.com>
2025-03-05 11:38:46 +0600

    delete file done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,7 +9,7 @@ const route = express.Router();
 route.post ("/create",create);
 route.get("/getAllUsers", fetch)
 route.put("/update/:id",update)
-
+route.delete("/delete/:id",deleteUser)
 
 
 export default route;
